42f11af338639c7b57cd5a9ea8de031bcb4ce273
GithubPrankster <33995085+GithubPrankster@users.noreply.github.com>
2019-12-27 09:20:26 -0300

    Removed compile_it.sh as Makefile does better job. And kernel.h and kernel.c updated with complete port IO wrappers.

--- kernel/plibc/kernel.c
+++ kernel/plibc/kernel.c
@@ -11,3 +11,28 @@ uint8_t inb(uint16_t port){
                    : "Nd"(port) );
     return ret;
 }
+
+void outw(uint16_t port, uint16_t word){
+    asm volatile ( "outw %0, %1" : : "a"(word), "Nd"(port) );
+}
+
+uint16_t inw(uint16_t port){
+    uint16_t ret;
+    asm volatile ( "inw %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+
+void outl(uint16_t port, uint32_t dualword){
+    asm volatile ( "outl %0, %1" : : "a"(dualword), "Nd"(port) );
+}
+
+uint32_t inl(uint16_t port){
+    uint32_t ret;
+    asm volatile ( "inl %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
--- kernel/plibc/kernel.h
+++ kernel/plibc/kernel.h
@@ -3,7 +3,22 @@
 
 #include <stdint.h>
 
-extern void outb(unsigned short port, unsigned char byte);
+//Output byte into specified port.
+extern void outb(uint16_t port, uint8_t byte);
+
+//Recieve byte from specified port.
 extern uint8_t inb(uint16_t port);
+
+//Output double-byte into specified port.
+extern void outw(uint16_t port, uint16_t word);
+
+//Recieve double-byte from specified port.
+extern uint16_t inw(uint16_t port);
+
+//Output long into specified port.
+extern void outl(uint16_t port, uint32_t dualword);
+
+//Recieve long from specified port.
+extern uint32_t inl(uint16_t port);
 
 #endif
